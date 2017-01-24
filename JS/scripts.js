d150e6a53dd90b572ff60683084bf59680ad04f0
Jiwon Kang and Minh Phuong <jiwon@>
2017-01-24 10:42:49 -0800

    Add constructor business logic

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,13 @@
 //BUSINESS LOGIC
+//Create constructor
+function Choice(movie,time,age) = {
+  this.movie = movie;
+  this.time = time;
+  this.age = age;
+}
 
 //USER INTERFACE
-$(document).ready(function(){
-
+$(document).ready(function();
   $("form").submit(function(event){
     event.preventDefault();
 
