e754b0c76c35ef2ef850b0fc3ec3908257a1aa41
Jiwon Kang and Minh Phuong <minh@>
2017-01-24 16:41:43 -0800

    Modify JS using nested objects

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,10 +2,11 @@
 var ticketPrice = 0;
 var discount = "";
 //Create constructor
-function Choice(movie,time,age) {
+function Choice(movie,time,age,quantity) {
   this.movie = movie;
-  this.time = time;
-  this.age = age;
+  this.time = parseInt(time);
+  this.age = parseInt(age);
+  this.quantity = parseInt(quantity);
 }
 //Create prototype to determine price on movie choice
 Choice.prototype.moviePrice = function () {
@@ -36,28 +37,38 @@ Choice.prototype.agePrice = function() {
   } else { ticketPrice-=5; discount = "senior"; }
   return ticketPrice;
 }
+//Create prototype to determine price on quantity
+Choice.prototype.quantityPrice = function() {
+  console.log(this.quantity);
+  return ticketPrice = ticketPrice * this.quantity;
+}
 //USER INTERFACE
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
+
     //Define variable value with user input
-    var inputtedMovie = $("input:radio[name=movies]:checked").val();
-    var inputtedTime = parseInt($("select#showtime").val());
-    var inputtedAge = parseInt($("input#age").val());
-    //Make new object using constructor and varibles
-    var userChoice = new Choice(inputtedMovie, inputtedTime, inputtedAge);
-    if (inputtedAge < 0) {
-      alert("Please enter a valid age");
-    } else {
-      userChoice.moviePrice();
-      userChoice.timePrice();
-      userChoice.agePrice();
-      console.log(ticketPrice);
-      $(".results").show();
-      $("#results").append(ticketPrice);
-      $(".movie").text($("input:radio[name=movies]:checked").val());
-      $(".time").text($("#showtime option:selected").text());
-      $(".age").text(discount);
-    }
+    $(".information").each(function() {
+      var inputtedMovie = $(this).find("input:radio[name=movies]:checked").val();
+      var inputtedTime = $(this).find("select#showtime").val();
+      var inputtedAge = $(this).find("input#age").val();
+      var inputtedQuantity = $(this).find("select#quantity").val();
+      //Make new object using constructor and varibles
+      var userChoice = new Choice(inputtedMovie, inputtedTime, inputtedAge,inputtedQuantity);
+
+      if (inputtedAge < 0) {
+        alert("Please enter a valid age");
+      } else {
+        userChoice.moviePrice();
+        userChoice.timePrice();
+        userChoice.agePrice();
+        userChoice.quantityPrice();
+        console.log(ticketPrice);
+        $(".results").show();
+        $("ul#results").append("<li>"+ "Your total is $ " + ticketPrice + " with the total of " + inputtedQuantity + " tickets " + " ;and you are watching " + inputtedMovie + " at " + $("#showtime option:selected").text() + " at " + discount + " price " + "</li>");
+      }
+    });
+
+
   });
 });
